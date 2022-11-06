b0e0ef215dcd28de01a6c30eb38317bcdb2eb8b0
Mostafa Aly <maly@oppa.app>
2022-11-06 21:22:36 +0200

    install styled-components library + add header logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,21 @@
 import { FC } from "react";
+import styled from "styled-components";
+import Logo from "./Logo";
+
+const StyledHeader = styled.header`
+  height: 56px;
+  display: flex;
+  @media (max-width: 768px) {
+    background: red;
+  }
+`;
 
 const Header: FC = () => {
-  return <header></header>;
+  return (
+    <StyledHeader>
+      <Logo />
+    </StyledHeader>
+  );
 };
 
 export default Header;
