fee18d7c16bfb4e9b584938ed3bb2bb3c036c246
Mostafa Aly <maly@oppa.app>
2022-11-08 21:42:55 +0200

    fix minor issue

--- src/utils/convertDuration.ts
+++ src/utils/convertDuration.ts
@@ -1,7 +1,7 @@
 const getDurationValue = (match: string[], unit: string): number => {
     const value = match.find((value) => value.includes(unit))
     if (!value) return 0;
-    return +(value.replace(/^\D+/g, '')[0]);
+    return parseInt(value.replace(/[^0-9]/g,''));
 }
 const appendHeadZero = (duration: number): string => duration <= 9 ? `0${duration}` : duration.toString();
 
@@ -11,7 +11,6 @@ export const convertDuration = (duration: string): string => {
     const hours = getDurationValue(match, "H");
     const minutes = getDurationValue(match, "M");
     const seconds = getDurationValue(match, "S");
-
     if (hours) return [hours, appendHeadZero(minutes), appendHeadZero(seconds)].join(":")
     return [minutes, appendHeadZero(seconds)].join(":")
 
